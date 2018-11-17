fa66a33ea28a7127ba318b22889bafb380b40666
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-16 23:35:21 -0500

    initial file set up - sets dummy info for db

---
+++ seed.sql
@@ -0,0 +1,22 @@
+
+INSERT INTO users (name, username, pwhash) VALUES ('Lauren', 'LilyLove', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK');
+INSERT INTO users (name, username, pwhash) VALUES ('Jackie', 'JAC', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK');
+INSERT INTO users (name, username, pwhash) VALUES ('Jessy', 'JayDeeBee', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK');
+INSERT INTO users (name, username, pwhash) VALUES ('April', 'Doodle', '$2b$10$QUJ346ylCYC7rIS2cbqEq.l4o.HCL6r.pZokcF6Tivha/UTXDi.JK');
+
+
+INSERT INTO entries (title, completed, date_completed, user_id) VALUES ('The Lies We Told', true, '20180321', 1);
+INSERT INTO entries (title, completed, date_completed, user_id) VALUES ('An Absolutely Remarkable Thing', true, '20180731', 2);
+INSERT INTO entries (title, completed, date_completed, user_id) VALUES ('A Ladder to The Sky', true, '20181206', 1);
+INSERT INTO entries (title, completed, date_completed, user_id) VALUES ('Winter In Paradise', true, '20181015' , 4);
+INSERT INTO entries (title, completed, date_completed, user_id) VALUES ('Nine Perfect Strangers', true, '20181108', 2);
+INSERT INTO entries (title, completed, date_completed, user_id) VALUES ('Cross Her Heart', true, '20180215', 3);
+INSERT INTO entries (title, completed, date_completed, user_id) VALUES ('The Proposal', true, '20180927', 4);
+INSERT INTO entries (title, completed, date_completed, user_id) VALUES ('The Air You Breathe', true, '20180822', 3);
+-- INSERT INTO entries (title, completed, date_completed, user_id) VALUES ('The Proposal', false, , 1);
+-- INSERT INTO entries (title, completed, date_completed, user_id) VALUES ('Circe', false, , 3);
+-- INSERT INTO entries (title, completed, date_completed, user_id) VALUES ('The Dinner List', false, , 4 );
+-- INSERT INTO entries (title, completed, date_completed, user_id) VALUES ('Nine Perfect Strangers', false, , 1);
+-- INSERT INTO entries (title, completed, date_completed, user_id) VALUES ('For Better and Worse', false, , 2 );
+-- INSERT INTO entries (title, completed, date_completed, user_id) VALUES ('Goodbye Paris', false, , 4);
+-- INSERT INTO entries (title, completed, date_completed, user_id) VALUES ('Circe', false, , 3);
